b37192da7dd99db8d18ad74bb47afc4a230e3e82
Anmol Raibhandare <raibhandare.anmol@gmail.com>
2020-09-01 15:41:43 -0700

    Tab Bar Controller Added.

---
+++ Kea/Cells/PetCell.swift
@@ -0,0 +1,17 @@
+//
+//  PetCell.swift
+//  Kea
+//
+//  Created by Anmol Raibhandare on 9/1/20.
+//  Copyright © 2020 Anmol Raibhandare. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PetCell: UITableViewCell {
+    @IBOutlet weak var pictureImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var animalLabel: UILabel!
+    @IBOutlet weak var dobLabel: UILabel!
+}
---
+++ Kea/Cells/UserCell.swift
@@ -0,0 +1,9 @@
+//
+//  UserCell.swift
+//  Kea
+//
+//  Created by Anmol Raibhandare on 9/1/20.
+//  Copyright © 2020 Anmol Raibhandare. All rights reserved.
+//
+
+import Foundation
